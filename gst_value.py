7ea441e5b7a2d8d84f3f979899f5bd479463cc78
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-18 21:36:56 +0530

    Create gst_value.py

---
+++ gst_value.py
@@ -0,0 +1,10 @@
+P=20000
+print("Purchase Amount:",P)
+SST=P*0.07
+print("SST@7%:",SST)
+CST=P*0.07
+print("CST@7%:",CST)
+GST=P*0.15
+print("GST@15%:",GST)
+Total_Bill=P+GST
+print("Total Bill:",Total_Bill)
