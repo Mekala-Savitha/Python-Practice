32a6bfedb4d4a046e929c18bfd6631ba7dac89a9
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-18 21:29:16 +0530

    Create type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,19 @@
+# Implicit type casting
+x=39
+y=64.19
+z=x+y
+print(z)
+print(type(z))
+
+# Explicit type casting 
+a=25.13
+b=45
+c=int(a+b)
+print(c)
+print(type(c))
+
+# List to Set
+s=[1,9,8,3]
+t=set(s)
+print(t)
+print(type(t))
