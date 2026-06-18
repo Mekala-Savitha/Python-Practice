4b5c3c8355cddc6c90cbf47dba11740aeba6cf20
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-18 20:07:32 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,48 @@
+# Numeric datatype
+age=89
+print(age)
+print(type(age))
+price=47.52
+print(price)
+print(type(price))
+x=35+8j
+print(x)
+print(type(x))
+
+# String datatype
+city="Hyderabad"
+print(city)
+print(type(city))
+letter='S'
+print(letter)
+print(type(letter))
+
+# Sequence datatype
+# List datatype
+numbers=[76,12.9,67+1j,"Rose"]
+numbers[2]=5+1j
+print(numbers)
+print(type(numbers))
+# Tuple datatype
+flowers=("Rose","Lotus","Lilly")
+print(type(flowers))
+# Range
+a=range(6)
+
+# Set datatype
+x={53,99.1,'C'}
+print(type(x))
+# Frozen set
+a={44,89,21}
+b=frozenset(a)
+print(b)
+print(type(b))
+
+# Dictionary datatype
+s={"x":"456","y":"90","12.15"}
+print(type(s))
+
+# Boolean datatype
+is_student=True
+print(is_student)
+print(type(is_student))
