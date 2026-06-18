cda71dbda5450f19bd63b87c7844fa636f730181
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-18 19:36:10 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,29 @@
+# Example 1
+name="Savitha"
+print(name)
+
+# Example 2
+age=22
+print("age:",age)
+
+# Example 3
+x=36
+y=87
+sum=x+y
+print(sum)
+
+#Example 4
+# Swap two variables
+a=23
+b=46
+a,b=b,a
+print("a=",a)
+print("b=",b)
+
+# Example 5
+name="Savitha"
+roll_no=539
+branch="CSE"
+print(name)
+print(roll_no)
+print(branch)
