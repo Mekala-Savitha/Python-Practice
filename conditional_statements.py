a60df0dfc97da641361c58532cd1f96a69069979
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-20 12:37:09 +0530

    Update conditional_statements.py

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,9 +1,19 @@
+"""
+CONDITIONAL STATEMENTS IN PYTHON 
+
+Definition:
+Conditional Statements allows a program to make decisions based on conditions.
+"""
+
 # if statement 
+# It allows the program to execute certain code,only when a condition is True.
 age=10
 if age<18:
   print("You are minor")
 
 # if-else statement 
+# The if-else statement is used to choose between two options.
+# It executes one block of code if the condition is True and another nlock of code if the condition is False.
 age=23
 if age<18:
   print("You are minor")
@@ -11,6 +21,7 @@ else:
   print("You are major")
 
 # if-elif-else statement 
+# This statement is used when you need to check multiple conditions.
 age=12
 if age<18:
   print("You are minor")
@@ -20,6 +31,7 @@ else:
   print("You are sr.citizen")
 
 # Nested-if statement 
+# A nested if statement means an if statement inside another if statement.
 rating=9
 if rating>5:
   if(rating>5) and (rating<8):
@@ -30,6 +42,7 @@ else:
   print("Bad")
   
 # Match-case statement 
+# Match case is a pattern matching statement,used to conpare a value against multiple patterns.
 x=2
 match x:
 	case 1:
