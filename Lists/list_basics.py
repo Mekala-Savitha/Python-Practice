b752ed241691504532b992dfe69a1e98ef9223a6
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-28 19:44:20 +0530

    Create list_basics.py

---
+++ Lists/list_basics.py
@@ -0,0 +1,41 @@
+# List Definition
+# A list is a built-in Python data structure used to store multiple
+# items in a single variable. Lists are ordered, mutable (changeable),
+# and allow duplicate values.
+
+# Syntax
+# list_name = [item1, item2, item3]
+
+# Creating Lists
+
+# Integer List
+numbers = [10, 20, 30, 40]
+print(numbers)
+
+# String List
+names = ["Savitha", "Radhika", "Nivrithi"]
+print(names)
+
+# Float List
+marks = [95.5, 88.0, 76.5]
+print(marks)
+
+# Mixed List
+data = [10, "Python", 3.14, True]
+print(data)
+
+# Empty List
+empty_list = []
+print(empty_list)
+
+# Nested List
+matrix = [[1, 2], [3, 4]]
+print(matrix)
+
+# Types of Lists
+# 1. Integer List
+# 2. String List
+# 3. Float List
+# 4. Mixed List
+# 5. Empty List
+# 6. Nested List
