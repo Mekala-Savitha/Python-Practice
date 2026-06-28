4a00205071947e8567af274a8729eeb8e8ad3f0f
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-28 19:46:50 +0530

    Create list_indexing.py

---
+++ Lists/list_indexing.py
@@ -0,0 +1,23 @@
+# List Indexing
+
+# Definition
+# List indexing is used to access individual elements of a list
+# using their position (index).
+
+# Positive Indexing
+# Positive indexing starts from 0 and moves from left to right.
+
+names = ["Savitha","Radhika", "Nivrithi","Rithanya"]
+
+print(names[0])   # Savitha
+print(names[1])   # Radhika
+print(names[2])   # Nivrithi
+print(names[3])   # Rithanya
+
+# Negative Indexing
+# Negative indexing starts from -1 and moves from right to left.
+
+print(names[-1])  # Rithanya
+print(names[-2])  # Nivrithi
+print(names[-3])  # Radhika
+print(names[-4])  # Savitha
