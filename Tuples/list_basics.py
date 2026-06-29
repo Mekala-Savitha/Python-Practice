39fae8f8ea2607ed50142e308b2c393746e5caa8
Mekala-Savitha <mekalasavitha202@gmail.com>
2026-06-29 10:59:09 +0530

    Create list_basics.py

---
+++ Tuples/list_basics.py
@@ -0,0 +1,27 @@
+# Tuple 
+# Definition:
+# A tuple is an ordered, immutable(can't be changed) collection of items.
+# Tuple allows duplicate values.
+
+# Syntax:
+# tuple_name=(item1,item2,item3)
+
+# Empty tuple 
+t=()
+print(t)
+
+# Single element tuple 
+t=(10,)
+print(t)
+
+# Multiple element tuple
+t=(10,15,30)
+print(t)
+
+# Using tuple
+t=tuple([1,5,9])
+print(t)
+
+# Mixed elements tuple
+t=(19,"savitha",39.4,'R')
+print(t)
